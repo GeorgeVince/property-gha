bdcb392e74ef0f152704c654a5365011150f1b19
George Vince <george.vince@hotmail.co.uk>
2022-01-27 13:47:13 +0000

    debugging

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
-import requests
-import json
-import csv
-from datetime import datetime
-import matplotlib.pylab as plt
+# import requests
+# import json
+# import csv
+
+# import matplotlib.pylab as plt
 import os
+
+from datetime import datetime
 
 IMAGE_NAME = "prices.png"
 DATA_NAME = "data.csv"
@@ -16,84 +18,84 @@ class ScriptTag:
     contents: str
 
 
-def _find_final_script_tag(content: str) -> ScriptTag:
-    script_start = content.rfind("<script")
-    content_start = content.find(">", script_start)
-    script_end = content.rfind("</script>")
+# def _find_final_script_tag(content: str) -> ScriptTag:
+#     script_start = content.rfind("<script")
+#     content_start = content.find(">", script_start)
+#     script_end = content.rfind("</script>")
 
-    return ScriptTag(contents=content[content_start + 1 : script_end])
-
-
-def _find_current_price_from_script_tag(scriptTag: ScriptTag):
-    """Dig through some nested JSON woop woop!"""
-    content = json.loads(scriptTag.contents)
-
-    # two keys we want the one thats not "ROOT_QUERY"
-    property_key = [
-        key
-        for key in content["props"]["pageProps"]["initialApolloState"].keys()
-        if key != "ROOT_QUERY"
-    ][0]
-
-    current_price = content["props"]["pageProps"]["initialApolloState"][property_key][
-        "details"
-    ]["estimate"]["sale"]["currentPrice"]
-    return current_price
+#     return ScriptTag(contents=content[content_start + 1 : script_end])
 
 
-def update_data(price: int):
+# def _find_current_price_from_script_tag(scriptTag: ScriptTag):
+#     """Dig through some nested JSON woop woop!"""
+#     content = json.loads(scriptTag.contents)
 
-    with open(DATA_NAME, "a") as csvfile:
-        fieldnames = ["date", "price"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerow({"date": TODAY, "price": price})
+#     # two keys we want the one thats not "ROOT_QUERY"
+#     property_key = [
+#         key
+#         for key in content["props"]["pageProps"]["initialApolloState"].keys()
+#         if key != "ROOT_QUERY"
+#     ][0]
 
-
-def _get_data_file_contents() -> list[dict[str, str]]:
-    with open(DATA_NAME) as csvfile:
-        fieldnames = ["date", "price"]
-        reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-        content = [r for r in reader]
-    return content
-
-
-def draw_and_export():
-    content = _get_data_file_contents()
-    x = [r["date"] for r in content]
-    y = [int(r["price"]) for r in content]
-    plt.figure(figsize=(15, 10))
-
-    plt.plot(x, y)
-    plt.xlabel("date")
-    plt.ylabel("estimated price")
-    plt.title("Zoopla Property Estimate")
-
-    plt.savefig(IMAGE_NAME)
+#     current_price = content["props"]["pageProps"]["initialApolloState"][property_key][
+#         "details"
+#     ]["estimate"]["sale"]["currentPrice"]
+#     return current_price
 
 
-def verify():
-    # Checks we have an out.png and data for todays date
-    file_present = os.path.exists(IMAGE_NAME)
-    content = _get_data_file_contents()
-    todays_date_present = any(r["date"] == TODAY for r in content)
+# def update_data(price: int):
 
-    print(f"::set-output name=completed::{file_present and todays_date_present}")
+#     with open(DATA_NAME, "a") as csvfile:
+#         fieldnames = ["date", "price"]
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#         writer.writerow({"date": TODAY, "price": price})
 
-def get_current_price():
-    property_uprn = os.getenv("INPUT_UPRN")
-    print(f"::set-output name=testOutput::{property_uprn}")
 
-    r = requests.get(f"https://www.zoopla.co.uk/property/uprn/{property_uprn}/")
-    content = r.text
-    tag = _find_final_script_tag(content)
-    return _find_current_price_from_script_tag(tag)
+# def _get_data_file_contents() -> list[dict[str, str]]:
+#     with open(DATA_NAME) as csvfile:
+#         fieldnames = ["date", "price"]
+#         reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+#         content = [r for r in reader]
+#     return content
 
-def main():
 
-    current_price = get_current_price()
-    update_data(current_price)
-    draw_and_export()
-    verify()
+# def draw_and_export():
+#     content = _get_data_file_contents()
+#     x = [r["date"] for r in content]
+#     y = [int(r["price"]) for r in content]
+#     plt.figure(figsize=(15, 10))
+
+#     plt.plot(x, y)
+#     plt.xlabel("date")
+#     plt.ylabel("estimated price")
+#     plt.title("Zoopla Property Estimate")
+
+#     plt.savefig(IMAGE_NAME)
+
+
+# def verify():
+#     # Checks we have an out.png and data for todays date
+#     file_present = os.path.exists(IMAGE_NAME)
+#     content = _get_data_file_contents()
+#     todays_date_present = any(r["date"] == TODAY for r in content)
+
+#     print(f"::set-output name=completed::{file_present and todays_date_present}")
+
+# def get_current_price():
+#     property_uprn = os.getenv("INPUT_UPRN")
+#     print(f"::set-output name=testOutput::{property_uprn}")
+
+#     r = requests.get(f"https://www.zoopla.co.uk/property/uprn/{property_uprn}/")
+#     content = r.text
+#     tag = _find_final_script_tag(content)
+#     return _find_current_price_from_script_tag(tag)
+
+# def main():
+
+#     current_price = get_current_price()
+#     update_data(current_price)
+#     draw_and_export()
+#     verify()
 
 if __name__ == "__main__":
     property_uprn = os.getenv("INPUT_UPRN")
